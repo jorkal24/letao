5b6f753c7d83aa555276f9335c3a5b4f24e7f11f
L.K <jorkal24@163.com>
2017-11-21 22:17:59 +0800

    退出功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,24 +1,43 @@
-
 // 进度条
-NProgress.configure({showSpinner: false});
+NProgress.configure({
+    showSpinner: false
+});
 
-$(function (){
-    $(document).ajaxStart(function (){
+$(function () {
+    $(document).ajaxStart(function () {
         NProgress.start();
     });
-    $(document).ajaxStop(function (){
-            NProgress.done();
+    $(document).ajaxStop(function () {
+        NProgress.done();
     })
 });
 
 // 二级下拉菜单的显示和隐藏
-$(".child").prev().on("click" , function (){
+$(".child").prev().on("click", function () {
     $(this).next().stop().slideToggle();
 });
 
 // 侧边栏的显示和隐藏
 
-$(".icon_menu").on("click" , function (){
+$(".icon_menu").on("click", function () {
     $(".lt_aside").toggleClass("now");
     $(".lt_main").toggleClass("now");
+});
+
+
+// 退出功能
+$(".icon_logout").on("click", function () {
+    $("#logoutModal").modal("show");
+
+    $(".btn_logout").off().on("click", function () {
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            success: function (data) {
+                if(data.success){
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
 })
