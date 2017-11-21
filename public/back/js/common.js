d799ecf0dbbfd4f68c4d776d700871a73dd271d5
L.K <jorkal24@163.com>
2017-11-21 21:45:20 +0800

    侧边栏及主体栏顶部完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,5 @@
 
+// 进度条
 NProgress.configure({showSpinner: false});
 
 $(function (){
@@ -9,3 +10,15 @@ $(function (){
             NProgress.done();
     })
 });
+
+// 二级下拉菜单的显示和隐藏
+$(".child").prev().on("click" , function (){
+    $(this).next().stop().slideToggle();
+});
+
+// 侧边栏的显示和隐藏
+
+$(".icon_menu").on("click" , function (){
+    $(".lt_aside").toggleClass("now");
+    $(".lt_main").toggleClass("now");
+})
